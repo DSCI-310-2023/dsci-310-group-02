a112b4e015501a1321a8ab8157d389dfa27f5628
kashish1928 <kashishjoshipura@gmail.com>
2023-03-10 10:32:51 -0800

    created a tests directory

---
+++ tests/testthat/test-data_summary.R
@@ -0,0 +1,13 @@
+source("../../R/data_summary.R")
+
+library(testthat)
+
+test_that("data_summary_fun returns the correct median", {
+  
+  # Call function with example data file path
+  actual_output <- data_summary_fun("https://archive.ics.uci.edu/ml/machine-learning-databases/wine-quality/winequality-red.csv")
+  
+  # Test that actual output matches expected output
+  expect_equal(actual_output$median, 10.2)
+  
+})
