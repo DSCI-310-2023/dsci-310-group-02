ee75215be8e3808e844276c978d43a5237fa7b7e
kashish1928 <kashishjoshipura@gmail.com>
2023-03-10 11:04:55 -0800

    resolved the error in data_summary.R

--- R/data_summary.R
+++ R/data_summary.R
@@ -1,16 +1,46 @@
+#' Summarize the data
+#'
+#' Creates a new data frame with three columns 
+#' (mean, median and standard deviation) 
+#' calculated from a particular column 
+#' of a data frame.
+#'
+#' @param file_path_url A string with the URL or path to the file.
+#' @param col A string with the quoted name of the column to summarize.
+#'
+#' @return A data frame with three columns 
+#'   (median, mean and standard deviation).
+#'
+#' @export
+#'
+#' @examples
+#' data_summary_fun("https://archive.ics.uci.edu/ml/machine-learning-databases/wine-quality/winequality-white.csv","alcohol")
+#' 
 
 library(tidyverse)
 
-data_summary_fun <- function(file_path_url) {
-    data <- read_delim(file_path_url, ";")
-    new_names <- c("fixed_acidity", "volatile_acidity", "citric_acid", "residual_sugar", "chlorides", "free_sulfur_dioxide", "total_sulfur_dioxide", "density", "pH", "sulphates", "alcohol", "quality")
-    data <- rename(data, !!!setNames(names(data), new_names))
-    data_median <- median(data$alcohol)
-    data_sd<-sd(data$alcohol)
-    data_new <- tibble(data_median,data_sd)
-    data_new <- rename(data_new, !!!setNames(names(data_new), c("median","standard_deviation")))
-    return(data_new)
-    
+data_summary_fun <- function(file_path_url, col) {
+  data <- read_delim(file_path_url, ";")
+  new_names <- c("fixed_acidity", "volatile_acidity", "citric_acid", "residual_sugar","chlorides", "free_sulfur_dioxide", 
+                 "total_sulfur_dioxide", "density","pH", "sulphates", "alcohol", "quality")
+  data <- rename(data, !!!setNames(names(data), new_names))
+  
+  # Check if selected column exists
+  if (!(col %in% names(data))) {
+    stop(paste("Column", col, "does not exist"))
+  }
+  
+  # Check if selected column is numeric or not
+  if (!is.numeric(data[[col]])) {
+    stop(paste("Column", col, "not numeric"))
+  }
+  
+  data_mean <- mean(data[[col]], na.rm = TRUE)
+  data_median <- median(data[[col]], na.rm = TRUE)
+  data_sd <- sd(data[[col]], na.rm = TRUE)
+  data_new <- tibble(data_mean, data_median, data_sd)
+  data_new <- rename(data_new,!!!setNames(names(data_new), c("mean", "median", "standard_deviation")))
+  return(data_new)
 }
 
 
--- tests/testthat/test-data_summary.R
+++ tests/testthat/test-data_summary.R
@@ -5,7 +5,7 @@ library(testthat)
 test_that("data_summary_fun returns the correct median", {
   
   # Call function with example data file path
-  actual_output <- data_summary_fun("https://archive.ics.uci.edu/ml/machine-learning-databases/wine-quality/winequality-red.csv")
+  actual_output <- data_summary_fun("https://archive.ics.uci.edu/ml/machine-learning-databases/wine-quality/winequality-red.csv","alcohol")
   
   # Test that actual output matches expected output
   expect_equal(actual_output$median, 10.2)
