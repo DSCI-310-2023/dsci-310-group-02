1e600b4d1591f1ec3e7a0272794696bd03eaa6dd
Dispeter <leepeter718@gmail.com>
2023-03-22 22:08:39 -0700

    changes according to milestone 2 feedbacl

--- tests/testthat/test-data_split.R
+++ tests/testthat/test-data_split.R
@@ -2,22 +2,12 @@
 # Date: 10-03-2023
 
 set.seed(123)
-source("./splitdata.R")
+source("R/data_split.R")
 
-# importing necessary libraries
-library(testthat)
-library(tidyverse)
-library(tidymodels)
-
-data = read_delim("https://archive.ics.uci.edu/ml/machine-learning-databases/wine-quality/winequality-white.csv", ";")
-
-# splitting it into training 75% and testing 25% 
-split <- initial_split(data, prop = 0.75, strata = quality)
-training <- training(split)
 
 
 test_that("The training of the split data returns the same dataframe", {
   # comparing actual output with expected output 
-  expect_identical(splitdata("https://archive.ics.uci.edu/ml/machine-learning-databases/wine-quality/winequality-white.csv"), training)
+  expect_identical(splitdata("https://archive.ics.uci.edu/ml/machine-learning-databases/wine-quality/winequality-white.csv"), train)
 })
 
