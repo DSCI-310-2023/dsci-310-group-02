a4f784aa7eea5d33d742ae9efc31b1fea6db5b5f
Dispeter <leepeter718@gmail.com>
2023-03-22 22:10:22 -0700

    changes according to milestone 2 feedback

--- tests/testthat/test-data_split.R
+++ tests/testthat/test-data_split.R
@@ -4,8 +4,6 @@
 set.seed(123)
 source("R/data_split.R")
 
-
-
 test_that("The training of the split data returns the same dataframe", {
   # comparing actual output with expected output 
   expect_identical(splitdata("https://archive.ics.uci.edu/ml/machine-learning-databases/wine-quality/winequality-white.csv"), train)
